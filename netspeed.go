c9323c82c72ed187b214ff125df9dc475005ede9
gdanko <Gary_Danko@intuit.com>
2024-05-03 11:15:01 -0700

    Put version in internal

--- netspeed.go
+++ netspeed.go
@@ -9,18 +9,16 @@ import (
 	"time"
 
 	"github.com/gdanko/netspeed/globals"
+	"github.com/gdanko/netspeed/internal"
 	"github.com/gdanko/netspeed/iostat"
 	"github.com/gdanko/netspeed/output"
 	"github.com/gdanko/netspeed/util"
 )
 
-const VERSION = "0.2.0"
-
 func main() {
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan,
 		syscall.SIGINT,
-		syscall.SIGKILL,
 		syscall.SIGQUIT,
 		syscall.SIGTERM,
 	)
@@ -51,7 +49,7 @@ func main() {
 	opts := globals.Options{}
 
 	opts.Version = func() {
-		fmt.Printf("netspeed version %s\n", VERSION)
+		fmt.Printf("netspeed version %s\n", internal.Version(false, true))
 		util.ExitCleanly()
 	}
 
@@ -128,36 +126,4 @@ func main() {
 	}(ctx)
 
 	<-forever
-
-	// for {
-	// 	iostatDataNew, err := iostat.GetData()
-	// 	if err != nil {
-	// 		util.ExitOnError(err.Error())
-	// 	}
-	// 	for _, iostatBlock := range iostatDataNew {
-	// 		if iostatBlock.Interface == globals.GetInterfaceName() {
-	// 			newBytesSent = iostatBlock.BytesSent
-	// 			newBytesRecv = iostatBlock.BytesRecv
-	// 			newPacketsSent = iostatBlock.PacketsSent
-	// 			newPacketsRecv = iostatBlock.PacketsRecv
-	// 		}
-	// 	}
-
-	// 	netspeedData := globals.NetspeedData{
-	// 		Timestamp:   util.GetTimestamp(),
-	// 		Interface:   globals.GetInterfaceName(),
-	// 		KBytesSent:  (newBytesSent - oldBytesSent) / 1024,
-	// 		KBytesRecv:  (newBytesRecv - oldBytesRecv) / 1024,
-	// 		PacketsSent: newPacketsSent - oldPacketsSent,
-	// 		PacketsRecv: newPacketsRecv - oldPacketsRecv,
-	// 	}
-
-	// 	output.ProcessOutput(netspeedData)
-
-	// 	oldBytesSent = newBytesSent
-	// 	oldBytesRecv = newBytesRecv
-	// 	oldPacketsSent = newPacketsSent
-	// 	oldPacketsRecv = newPacketsRecv
-	// 	time.Sleep(1 * time.Second)
-	// }
 }
