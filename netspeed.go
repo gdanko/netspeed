499c91ced385bea2792a33a10ef00efb47187a01
gdanko <Gary_Danko@intuit.com>
2024-05-03 11:07:09 -0700

    Move a lot of the stuff into modules

--- netspeed.go
+++ netspeed.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
@@ -17,7 +18,12 @@ const VERSION = "0.2.0"
 
 func main() {
 	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(sigChan,
+		syscall.SIGINT,
+		syscall.SIGKILL,
+		syscall.SIGQUIT,
+		syscall.SIGTERM,
+	)
 
 	go func() {
 		sig := <-sigChan
@@ -49,7 +55,7 @@ func main() {
 		util.ExitCleanly()
 	}
 
-	err = util.ProcessOptions(opts, interfaceList)
+	err = util.ProcessOptions(opts)
 	if err != nil {
 		util.ExitOnError(err.Error())
 	}
@@ -73,35 +79,85 @@ func main() {
 	}
 	time.Sleep(1 * time.Second)
 
-	for {
-		iostatDataNew, err := iostat.GetData()
-		if err != nil {
-			util.ExitOnError(err.Error())
-		}
-		for _, iostatBlock := range iostatDataNew {
-			if iostatBlock.Interface == globals.GetInterfaceName() {
-				newBytesSent = iostatBlock.BytesSent
-				newBytesRecv = iostatBlock.BytesRecv
-				newPacketsSent = iostatBlock.PacketsSent
-				newPacketsRecv = iostatBlock.PacketsRecv
+	// globals.SetNetspeedChan(make(chan bool))
+	// defer close(globals.GetNetspeedChan())
+
+	forever := make(chan struct{})
+	ctx, cancel := context.WithCancel(context.Background())
+	globals.SetExitNetspeed(cancel)
+
+	go func(ctx context.Context) {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				for {
+					iostatDataNew, err := iostat.GetData()
+					if err != nil {
+						util.ExitOnError(err.Error())
+					}
+					for _, iostatBlock := range iostatDataNew {
+						if iostatBlock.Interface == globals.GetInterfaceName() {
+							newBytesSent = iostatBlock.BytesSent
+							newBytesRecv = iostatBlock.BytesRecv
+							newPacketsSent = iostatBlock.PacketsSent
+							newPacketsRecv = iostatBlock.PacketsRecv
+						}
+					}
+
+					netspeedData := globals.NetspeedData{
+						Timestamp:   util.GetTimestamp(),
+						Interface:   globals.GetInterfaceName(),
+						KBytesSent:  (newBytesSent - oldBytesSent) / 1024,
+						KBytesRecv:  (newBytesRecv - oldBytesRecv) / 1024,
+						PacketsSent: newPacketsSent - oldPacketsSent,
+						PacketsRecv: newPacketsRecv - oldPacketsRecv,
+					}
+
+					output.ProcessOutput(netspeedData)
+
+					oldBytesSent = newBytesSent
+					oldBytesRecv = newBytesRecv
+					oldPacketsSent = newPacketsSent
+					oldPacketsRecv = newPacketsRecv
+					time.Sleep(1 * time.Second)
+				}
 			}
 		}
+	}(ctx)
 
-		netspeedData := globals.NetspeedData{
-			Timestamp:   util.GetTimestamp(),
-			Interface:   globals.GetInterfaceName(),
-			KBytesSent:  (newBytesSent - oldBytesSent) / 1024,
-			KBytesRecv:  (newBytesRecv - oldBytesRecv) / 1024,
-			PacketsSent: newPacketsSent - oldPacketsSent,
-			PacketsRecv: newPacketsRecv - oldPacketsRecv,
-		}
+	<-forever
 
-		output.ProcessOutput(netspeedData)
+	// for {
+	// 	iostatDataNew, err := iostat.GetData()
+	// 	if err != nil {
+	// 		util.ExitOnError(err.Error())
+	// 	}
+	// 	for _, iostatBlock := range iostatDataNew {
+	// 		if iostatBlock.Interface == globals.GetInterfaceName() {
+	// 			newBytesSent = iostatBlock.BytesSent
+	// 			newBytesRecv = iostatBlock.BytesRecv
+	// 			newPacketsSent = iostatBlock.PacketsSent
+	// 			newPacketsRecv = iostatBlock.PacketsRecv
+	// 		}
+	// 	}
 
-		oldBytesSent = newBytesSent
-		oldBytesRecv = newBytesRecv
-		oldPacketsSent = newPacketsSent
-		oldPacketsRecv = newPacketsRecv
-		time.Sleep(1 * time.Second)
-	}
+	// 	netspeedData := globals.NetspeedData{
+	// 		Timestamp:   util.GetTimestamp(),
+	// 		Interface:   globals.GetInterfaceName(),
+	// 		KBytesSent:  (newBytesSent - oldBytesSent) / 1024,
+	// 		KBytesRecv:  (newBytesRecv - oldBytesRecv) / 1024,
+	// 		PacketsSent: newPacketsSent - oldPacketsSent,
+	// 		PacketsRecv: newPacketsRecv - oldPacketsRecv,
+	// 	}
+
+	// 	output.ProcessOutput(netspeedData)
+
+	// 	oldBytesSent = newBytesSent
+	// 	oldBytesRecv = newBytesRecv
+	// 	oldPacketsSent = newPacketsSent
+	// 	oldPacketsRecv = newPacketsRecv
+	// 	time.Sleep(1 * time.Second)
+	// }
 }
